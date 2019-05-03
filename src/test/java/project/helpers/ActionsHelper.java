1266348331d773eb4c791850f12463d5595dd9d9
cpanzaru <claudiudamian.panzaru@endava.com>
2019-05-03 14:55:58 +0300

    Added ActionsHelper class, selenium generic methods and created actionsHelper object in TestContext class

---
+++ src/test/java/project/helpers/ActionsHelper.java
@@ -0,0 +1,38 @@
+package project.helpers;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import project.TestContext;
+
+public class ActionsHelper {
+    private WebDriver getDriver() {
+        return TestContext.getDriver();
+    }
+
+    public void navigateTo(String link) {
+        getDriver().get(link);
+    }
+
+    public WebElement getElement(By by) {
+        return getDriver().findElement(by);
+    }
+
+    public void clickElement(By by) {
+        getElement(by).click();
+    }
+
+    public void isElementDisplayed(By by) {
+        getElement(by).isDisplayed();
+    }
+
+    public void sendStringKeysToElement(By by, String keys) {
+        getElement(by).sendKeys(keys);
+    }
+
+    public void sendKeysToElement(By by, Keys keys) {
+        getElement(by).sendKeys(keys);
+    }
+
+}
