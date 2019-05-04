3b1ffad349730c7029ba1494c65d696745c20516
cpanzaru <claudiudamian.panzaru@endava.com>
2019-05-04 16:05:18 +0300

    Changed shouldContain method, added target folder to gitignore

--- src/test/java/project/helpers/AssertHelper.java
+++ src/test/java/project/helpers/AssertHelper.java
@@ -4,7 +4,7 @@ import org.hamcrest.CoreMatchers;
 import org.junit.Assert;
 
 public class AssertHelper {
-    public void shouldContain(String expected, String actual) {
+    public void shouldContain(String actual, String expected) {
         Assert.assertThat(expected,
                 CoreMatchers.containsString(actual));
     }
