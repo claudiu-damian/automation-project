efd9bd9d719cc4808daee5033cc079cc600480e2
Sergiu Gutu <sergiu.gutu@endava.com>
2019-05-07 15:34:12 +0300

    Update of Logger Helper and Added SearchResultPage class

--- src/test/java/project/helpers/LoggerHelper.java
+++ src/test/java/project/helpers/LoggerHelper.java
@@ -1,8 +1,21 @@
 package project.helpers;
 
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.logging.FileHandler;
+import java.util.logging.Handler;
+import java.util.logging.LogManager;
 import java.util.logging.Logger;
 
 public class LoggerHelper {
-    public static final Logger logger = Logger.getGlobal();
+    public static final Logger logger = Logger.getLogger(LoggerHelper.class.getName());
+    public void setLogger() throws IOException {
+        LogManager.getLogManager().readConfiguration();
+//        Handler consoleHandler = new ConsoleHandler();
+        Handler fileHandler = new FileHandler("%h/MyJavaLog.log");
+        logger.addHandler(fileHandler);
+        System.out.println(System.getProperty("java.util.logging.config.file"));
+    }
 }
 
