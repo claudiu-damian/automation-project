434a6e3cc58b7230150331664ffcfb9af0f41662
Sergiu Gutu <sergiu.gutu@endava.com>
2019-05-10 14:26:44 +0300

    Integrated cucmber logger

--- src/test/java/CucumberRunner.java
+++ src/test/java/CucumberRunner.java
@@ -6,9 +6,15 @@ import org.junit.runner.RunWith;
 
 @CucumberOptions(
         features = {"src/test/resources/features/"},
-        tags = "@Claudiu",
+        tags = {"@Claudiu", "~@wip","~@notImplemented","@sanity"},
         glue = {"project.hooks",
-                "steps"}
+                "steps","com.StepDefinitions"},
+        dryRun = false,
+        strict = true,
+        monochrome = true,
+        plugin = { "progress",
+                "html:target/Cucumber"
+        }
 )
 
 public class CucumberRunner {
